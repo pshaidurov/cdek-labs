d76c32d2e281ab86ab5b16e5c5a449910b0c28cb
p.shaydurov <psh@cdek.ru>
2023-06-30 13:19:32 +0500

    calc

---
+++ main.java
@@ -0,0 +1,82 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String userInput = "";
+        while (!"q".equals(userInput)) {
+
+            try {
+                System.out.println("Enter number one:");
+                int firstNumber = scanner.nextInt();
+                System.out.println("Enter number two:");
+                int secondNumber = scanner.nextInt();
+                System.out.println("Enter operation or press q to quit:");
+                scanner.nextLine();
+                userInput = scanner.nextLine();
+                double result;
+
+
+                switch (userInput) {
+                    case "+":
+                        result = plus(firstNumber, secondNumber);
+                        break;
+                    case "-":
+                        result = minus(firstNumber, secondNumber);
+                        break;
+                    case "*":
+                        result = ymn(firstNumber, secondNumber);
+                        break;
+                    case "/":
+                        if (secondNumber == 0) {
+                            continue;
+                        }
+                        result = del(firstNumber, secondNumber);
+                        break;
+                    case "!":
+                        result = fact(firstNumber);
+                        break;
+                    default:
+                        result = 0;
+                        break;
+
+                }
+                System.out.println("Result: " + result);
+            } catch (InputMismatchException ex) {
+                System.out.println("Wrong input data!");
+                scanner.nextLine();
+                continue;
+
+            }
+        }
+    }
+
+    private static int plus(int numberOne, int numberTwo) {
+        return numberOne + numberTwo;
+    }
+
+    private static int minus(int numberOne, int numberTwo) {
+        return numberOne - numberTwo;
+    }
+
+    private static int ymn(int numberOne, int numberTwo) {
+        return numberOne * numberTwo;
+    }
+
+    private static int del(int numberOne, int numberTwo) {
+        return numberOne / numberTwo;
+    }
+
+    private static int fact(int numberOne) {
+        int res = 1;
+        for (int i = 1; i < numberOne; i++) {
+            res *= i;
+        }
+        return res;
+    }
+
+
+}
